8f5b1648f32841e6a87062031514ae7f273e443f
mahbubur-rahman-rambird <mahbubur-rahman-rambird@gmail.com>
2015-03-07 00:51:15 +0000

    First commit

---
+++ src/main/java/com/rambird/model/User.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rambird.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.core.style.ToStringCreator;
+
+/**
+ * Simple JavaBean domain object representing an owner.
+ *
+ * @author Ken Krebs
+ * @author Juergen Hoeller
+ * @author Sam Brannen
+ * @author Michael Isvy
+ */
+@Entity
+@Table(name = "myoath")
+public class User  {
+    @Column(name = "user_name")
+    @NotEmpty
+    private String userName;
+
+    @Column(name = "full_name")
+    @NotEmpty
+    private String fullName;
+
+    @Column(name = "home")
+    @NotEmpty
+    private String home;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+
+    public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+    public String toString() {
+        return new ToStringCreator(this)
+
+                .append("userName", this.getUserName())
+                .append("fullName", this.getFullName())
+                .append("home", this.getHome())
+                .toString();
+    }
+}
